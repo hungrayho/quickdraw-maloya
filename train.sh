d5d3add3a1b8306b792f71d67df530037c9a6945
Hung Ray Ho <hunkinhr@gmail.com>
2020-05-04 19:24:17 +0800

    train sh

---
+++ train.sh
@@ -0,0 +1,4 @@
+python train_model.py \
+    --training_data=data/quickdraw_dataset_v1/training.tfrecord-?????-of-????? \
+    --eval_data=data/quickdraw_dataset_v1/eval.tfrecord-?????-of-????? \
+    --classes_file=data/quickdraw_dataset_v1/training.tfrecord.classes
